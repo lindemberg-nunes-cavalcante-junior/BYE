6846af32a1889ac9fe4374bb0bf0ff4ce5a3f22f
Ikuvfgrs <31578691+Ikuvfgrs@users.noreply.github.com>
2025-07-27 17:08:20 -0300

    Delete BYE directory

--- BYE/ISP/email/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-postconf -e "myhostname = bye.email"
-postconf -e "mydestination = \$myhostname, localhost.\$mydomain, localhost"
-postconf -e "inet_interfaces = all"
-postconf -e "inet_protocols = all"
-
-/usr/sbin/postfix start-fg
