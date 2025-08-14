a3108e82a5713fd7fa50a77a4f98cc0ef15fb490
Lindemberg Júnior <lindembergjnr@gmail.com>
2025-08-14 20:30:15 -0300

    feito

--- stop.sh
+++ stop.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Salva o diretório atual
-CURDIR=$(pwd)
+docker compose down
 
-# Função para derrubar compose em uma pasta
-down_compose() {
-  echo "Parando serviços em $1"
-  cd "$1" || exit
-  docker compose down
-  cd "$CURDIR" || exit
-}
-
-down_compose "Cliente1_Pedro"
-down_compose "Cliente2_Lindemberg"
-down_compose "Cliente3_Matheus"
-down_compose "ISP"
-
-echo "Todos os serviços foram parados!"
-
+echo "Serviços foram parados"
